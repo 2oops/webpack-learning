95dfd9bd3cdeae103a44c3e3c5ecbc0019a2d262
2oops <yxlaimee@163.com>
2020-02-21 23:17:53 +0800

    inherit

--- example/node/inherit.js
+++ example/node/inherit.js
@@ -28,3 +28,18 @@ class Manager extends Person {
 
 let m1 = new Manager('2oops', 20, 125, 200000)
 m1.getSalary()  // 2oops 120 200000
+
+
+// 借用构造函数继承
+// 在子类构造函数中通过call()调用父类构造函数
+function Car(name, age) {
+  this.name = name
+  this.age = age
+  this.getName = function() {}
+}
+Car.prototype.setAge = function() {}
+function Taxi(name, age, price) {
+  Car.call(this, name, age)
+  this.price = price
+}
+let taxi = new Taxi('tesla', 10, 1000)
